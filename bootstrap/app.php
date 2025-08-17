fdd1f1964c92a5d74fc049121e9a05d76b181205
ABDALRZAQ345 <jmalj6564@gmail.com>
2025-08-17 12:01:50 +0300

    D

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -37,64 +37,64 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
 
-        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
-
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Object not found.',
-                ], 404);
-            }
-
-        });
-        $exceptions->render(function (AuthorizationException $e, Request $request) {
-
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Unauthorized.',
-                ], 404);
-            }
-
-        });
-        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
-
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Forbidden',
-                ], 403);
-            }
-        });
-        $exceptions->render(function (ThrottleRequestsException $e, Request $request) {
-
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Too Many Attempts try again later .',
-                ], 429);
-            }
-        });
-        $exceptions->render(function (\Illuminate\Validation\UnauthorizedException | \App\Exceptions\UNAuthorizedException $e, Request $request) {
-
-            if ($request->is('api/*')) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Unauthenticated',
-                ], 401);
-            }
-        });
-
-//        if (app()->environment() == 'production') {
-//            $exceptions->render(function (Exception $e, Request $request) {
-//                if ($request->is('api/*')) {
-//                    return response()->json([
-//                        'status' => false,
-//                        'message' => 'Something went wrong.',
-//                    ], 500);
-//                }
-//            });
-//        }
+//        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+//
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'status' => false,
+//                    'message' => 'Object not found.',
+//                ], 404);
+//            }
+//
+//        });
+//        $exceptions->render(function (AuthorizationException $e, Request $request) {
+//
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'status' => false,
+//                    'message' => 'Unauthorized.',
+//                ], 404);
+//            }
+//
+//        });
+//        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
+//
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'status' => false,
+//                    'message' => 'Forbidden',
+//                ], 403);
+//            }
+//        });
+//        $exceptions->render(function (ThrottleRequestsException $e, Request $request) {
+//
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'status' => false,
+//                    'message' => 'Too Many Attempts try again later .',
+//                ], 429);
+//            }
+//        });
+//        $exceptions->render(function (\Illuminate\Validation\UnauthorizedException | \App\Exceptions\UNAuthorizedException $e, Request $request) {
+//
+//            if ($request->is('api/*')) {
+//                return response()->json([
+//                    'status' => false,
+//                    'message' => 'Unauthenticated',
+//                ], 401);
+//            }
+//        });
+//
+////        if (app()->environment() == 'production') {
+////            $exceptions->render(function (Exception $e, Request $request) {
+////                if ($request->is('api/*')) {
+////                    return response()->json([
+////                        'status' => false,
+////                        'message' => 'Something went wrong.',
+////                    ], 500);
+////                }
+////            });
+////        }
 
     })
     ->create();
