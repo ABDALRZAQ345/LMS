9ce0071ed9515a11df7b8a2694afd922e0ed3754
ABDALRZAQ345 <jmalj6564@gmail.com>
2025-08-21 18:48:24 +0300

    Sto

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -18,7 +18,7 @@ function  getPhoto($image)
      return $image
         ? (str_starts_with($image, 'https://via.placeholder.com')
         ? $image
-        : config('app.url') . $image)
+        : Storage::get($image) )
         : null;
 }
 if (! function_exists('DeletePublicPhoto')) {
