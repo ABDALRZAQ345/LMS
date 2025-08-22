1f2b38b8692a6fe29d1ed05fa0533e77daacc882
ABDALRZAQ345 <jmalj6564@gmail.com>
2025-08-22 21:35:26 +0300

    auth with github

--- app/Http/Controllers/Auth/GithubController.php
+++ app/Http/Controllers/Auth/GithubController.php
@@ -6,9 +6,14 @@ use App\Exceptions\ServerErrorException;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Responses\LogedInResponse;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GithubController extends Controller
 {
@@ -42,5 +47,45 @@ class GithubController extends Controller
             throw new ServerErrorException($e->getMessage());
         }
 
+    }
+    /**
+     * @throws ServerErrorException
+     * @throws \Throwable
+     */
+    public function handleGithubUser($token): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $githubUser = Socialite::driver('github')->stateless()->userFromToken($token);
+
+            $user = User::firstOrCreate(
+                [
+                    'email' => $githubUser->getEmail(),
+                ],
+                [
+                    'name'      => $githubUser->getName() ?? $githubUser->getNickname(),
+                    'github_id' => $githubUser->getId(),
+                    'password'  => Hash::make(Str::random(24)),
+                ]
+            );
+
+            DB::commit();
+
+            return LogedInResponse::response($user);
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            throw new ServerErrorException($exception->getMessage());
+        }
+    }
+
+    public function auth(Request $request): JsonResponse
+    {
+        $token = $request->input('access_token');
+
+        if (! $token) {
+            return response()->json(['error' => 'No token provided'], 400);
+        }
+
+        return $this->handleGithubUser($token);
     }
 }
